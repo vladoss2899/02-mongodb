74bf131107f1d11a99d024088d9c5bcdee904757
vladoss2899 <136236185+vladoss2899@users.noreply.github.com>
2026-01-09 21:27:44 +0100

    new

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -4,12 +4,10 @@ export const errorHandler = (err, req, res, next) => {
   let status = 500;
   let message = 'Internal Server Error';
 
-  // Якщо це помилка HttpError, беремо її статус та повідомлення
   if (err instanceof HttpError) {
     status = err.status;
     message = err.message;
   } else if (err.message) {
-    // Якщо це не HttpError, але має message
     message = err.message;
   }
 
