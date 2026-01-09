fba1e959a6728eb3025874839f1c72ed79c8d2d0
vladoss2899 <136236185+vladoss2899@users.noreply.github.com>
2026-01-09 20:30:23 +0100

    new

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+
+import notesRoutes from './routes/notesRoutes.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(logger);
+app.use(express.json());
+app.use(cors());
+
+app.use(notesRoutes);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+await connectMongoDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
